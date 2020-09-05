d50361d0bd57821ff8123cc88344aebd0a1d5b4c
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-05 02:26:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,13 +25,12 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
-  sharedValues = planeteer_calls.select {|n| valid_calls.include(n)}
+  sharedValues = planeteer_calls.filter {|n| valid_calls.include(n)}
 
   if sharedValues[0]
     return sharedValues[0]
   else
     return nil
-
 
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
